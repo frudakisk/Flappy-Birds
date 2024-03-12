5a57c63b72562356db521d548493e596d16bcf11
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-12 15:48:03 -0600

    player tube collision
    When the player hits a tube, we are notified of this

--- FlappyBirds/Assets/Scripts/PlayerController.cs
+++ FlappyBirds/Assets/Scripts/PlayerController.cs
@@ -41,4 +41,12 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector2.down * bounceForce, ForceMode2D.Impulse);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Tube"))
+        {
+            Debug.Log("Player hit the tube. Game Over!");
+        }
+    }
 }
