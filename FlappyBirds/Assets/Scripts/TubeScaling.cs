36ea106971f3e9c1b056cf05262df9c5c607419e
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-12 15:42:23 -0600

    simplified "scaling"
    I figured that trying to scale these two items very precisely was too difficult and not the main objective of this project. So, instead of scaling the two pipes, we just made the pipes longer and move the center of the parent container up and down to make it look like the pipes are scaling with each other. The gap between the pipes is always the same which is perfect for predictability.

--- FlappyBirds/Assets/Scripts/TubeScaling.cs
+++ FlappyBirds/Assets/Scripts/TubeScaling.cs
@@ -4,55 +4,28 @@ using UnityEngine;
 
 public class TubeScaling : MonoBehaviour
 {
-    public float strechFactor = 2f;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 5f;
+        TubePosition();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(CompareTag("TopTube"))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                TransformTube(-1, -0.5f, Vector3.up);
-            }
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                TransformTube(1, 0.5f, Vector3.down);
-            }
-        }
-
-        if(CompareTag("BottomTube"))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                TransformTube(1, -0.5f, Vector3.up);
-            }
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                TransformTube(-1, 0.5f, Vector3.down);
-            }
-        }
-
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 
-    private void TransformTube(float yScaleTransform, float yPosTransform, Vector3 openingTranslateVector)
+    private void TubePosition()
     {
-        Transform tube = transform.Find("Body");
-        Vector3 localScale = tube.localScale;
-        Vector3 pos = tube.position;
-        localScale.y += yScaleTransform;
-        pos.y -= yPosTransform;
-        tube.localScale = localScale;
-        tube.position = pos;
-
-        Transform opening = transform.Find("Opening");
-        opening.Translate(openingTranslateVector);
+        //determine position when creating tubes
+        float randomMiddle = Random.Range(-2f, 3f);
+        Vector3 pos = transform.position;
+        pos.y = randomMiddle;
+        transform.position = pos;
     }
+
 }
