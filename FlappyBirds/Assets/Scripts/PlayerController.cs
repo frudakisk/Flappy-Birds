3ef7018623c3aa341ff58e99405d73c33bfc4bf1
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-11 23:29:37 -0600

    simple player mechanics
    created a simple sprite player and gave it the flying mechanics

---
+++ FlappyBirds/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float impulseForce;
+    private float roofValue;
+    private float bounceForce;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        impulseForce = 10f;
+        roofValue = 4.5f;
+        bounceForce = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        CheckRoof();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * impulseForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckRoof()
+    {
+        if(transform.position.y >= roofValue)
+        {
+            transform.position = new Vector2(transform.position.x, roofValue);
+            rb.AddForce(Vector2.down * bounceForce, ForceMode2D.Impulse);
+        }
+    }
+}
