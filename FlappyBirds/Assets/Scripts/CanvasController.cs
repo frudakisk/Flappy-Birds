62657f3f7ab8e8077f3b3c9804740b6c9ec3f3ee
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-12 22:41:40 -0600

    UI elements
    added score functionality for game over panel and made a restart function for the restart button

--- FlappyBirds/Assets/Scripts/CanvasController.cs
+++ FlappyBirds/Assets/Scripts/CanvasController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CanvasController : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = $"Score\n{gameManager.points}";
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
